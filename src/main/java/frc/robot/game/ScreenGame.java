ab1667fa7ef0151529311c551aa727cf60e0c869
Littledog1229 <buildmine202@gmail.com>
2023-05-02 19:51:24 -0400

    More Overridables

--- src/main/java/frc/robot/game/ScreenGame.java
+++ src/main/java/frc/robot/game/ScreenGame.java
@@ -5,6 +5,9 @@ import frc.robot.subsystems.LEDController;
 public abstract class ScreenGame {
     public String getIdentifier() { return "none"; }
 
+    public void init() { }
+    public boolean isFinished() { return true; }
+    
     public void update() { }
     public void draw(LEDController controller) { }
     public void povUpdate(int controller, int direction) { }
